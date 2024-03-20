b6464c33f1fa6f4bde1544d4337104b3ab1c0ce2
Pedro Gabriel <53580539+salespedrogabriel@users.noreply.github.com>
2024-03-20 11:30:39 -0300

    [LAB1] Project 1 - One Bit Comparator

---
+++ [LAB1] Bit Comparators/[1.1] One Bit Comparator/one_bit.vhd
@@ -0,0 +1,49 @@
+----------------------------------------------------------------------------------
+-- University: PUC-Rio
+-- Discipline: ENG1448 - Computação Digital
+-- Author: Pedro Gabriel Serodio Sales
+-- 
+-- Create Date:    10:03:10 03/11/2024 
+-- Evaluation Development Board: Spartan-3E Starter Board
+-- Preferred Language: VHDL
+-- Source Name: 	 VHDL Module
+-- Module Name:    single_bit - Behavioral 
+-- Project Name: 	 1_1_Project_Single_Bit_Comparator
+-- Description: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity single_bit is
+    Port ( A : in  STD_LOGIC;
+           B : in  STD_LOGIC;
+           Z : out  STD_LOGIC);
+end single_bit;
+
+architecture Behavioral of single_bit is
+
+	signal comp1,comp2 : std_logic := '0';
+
+begin
+	
+	comp1 <= (A AND B);
+	comp2 <= (NOT A AND NOT B);
+	Z<= comp1 OR comp2;
+
+
+end Behavioral;
+
---
+++ [LAB1] Bit Comparators/[1.1] One Bit Comparator/one_bit_tb.vhd
@@ -0,0 +1,85 @@
+--------------------------------------------------------------------------------
+-- University: PUC-Rio
+-- Discipline: ENG1448 - Computação Digital
+-- Author: Pedro Gabriel Serodio Sales
+--
+-- Create Date:   10:03:38 03/11/2024
+-- Evaluation Development Board: Spartan-3E Starter Board
+-- Preferred Language: VHDL
+-- Source Name: 	 VHDL Module
+-- Module Name:    single_bit - Behavioral 
+-- Project Name: 	 [1.1] Single Bit Comparator  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: single_bit
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY single_bit_tb IS
+END single_bit_tb;
+ 
+ARCHITECTURE behavior OF single_bit_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT single_bit
+    PORT(
+         A : IN  std_logic;
+         B : IN  std_logic;
+         Z : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal A : std_logic := '0';
+   signal B : std_logic := '0';
+
+ 	--Outputs
+   signal Z : std_logic;
+   -- No clocks detected in port list. Replace clock below with 
+   -- appropriate port name 
+ 
+   constant clock_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: single_bit PORT MAP (
+          A => A,
+          B => B,
+          Z => Z
+        );
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+		a <= '0';
+		b <= '0';
+		wait for 100 ns;	
+		a <= '1';
+		b <= '0';
+		wait for 100 ns;	
+		a <= '0';
+		b <= '1';
+		wait for 100 ns;	
+		a <= '1';
+		b <= '1';
+      wait for 100 ns;
+
+      wait;
+   end process;
+
+END;
